128b8aae80467941e6ab012e93c53f5a9ee4cc93
davidisaacsurvive <40686166+ruggedwizard@users.noreply.github.com>
2026-04-27 17:18:43 +0100

    added signals and middleware

---
+++ base/email.py
@@ -0,0 +1,6 @@
+
+def send_welcome_email(email):
+    print(f"Sending welcome email to {email}...")
+    # Here you would integrate with an actual email service like SendGrid, Mailgun, etc.
+    # For this example, we'll just simulate the email sending process.
+    print(f"Welcome email sent to {email} successfully!")
--- todo_auth_apis/settings.py
+++ todo_auth_apis/settings.py
@@ -42,16 +42,19 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'drf_spectacular',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'base.middleware.LoggingMiddleware',
 ]
 
 ROOT_URLCONF = 'todo_auth_apis.urls'
@@ -158,3 +161,8 @@ SPECTACULAR_SETTINGS = {
     'SERVE_INCLUDE_SCHEMA': False,
     # OTHER SETTINGS
 }
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "https://david-todo-app-frontend.vercel.app",
+]
