d736084266f9cd4a47c26e080b0c8ef1c28d4039
davidisaacsurvive <40686166+ruggedwizard@users.noreply.github.com>
2026-04-29 18:30:13 +0100

    change settings

--- todo_auth_apis/wsgi.py
+++ todo_auth_apis/wsgi.py
@@ -13,6 +13,4 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todo_auth_apis.settings')
 
-app = get_wsgi_application()
-
-application = app
+ application = get_wsgi_application()
